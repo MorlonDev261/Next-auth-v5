0c97f2ee9fb242e5e27e35f68b1d98b5f0485f68
MorlonDev261 <mikandrapp@gmail.com>
2025-03-30 19:34:40 +0300

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col justify-center items-center bg-gradient-to-r from-sky-400 via-blue-500 to-blue-800">
+    <div className="flex min-h-screen flex-col justify-center items-center bg-gradient-to-b from-sky-400 via-blue-500 to-blue-800">
       {children}
     </div>
   );
