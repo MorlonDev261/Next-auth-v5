1bbd73ea2cec7128d5c955f61fd789fab91ba0f7
MorlonDev261 <mikandrapp@gmail.com>
2025-03-30 19:55:58 +0300

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import LoginForm from "@/components/login-form";
 
 const BasicComponent = () => {
   return (
     <div className="p-4 bg-gray-100 dark:bg-gray-800 rounded-lg shadow">
       <h1 className="text-xl font-semibold text-gray-900 dark:text-white">
-        Login
+        <LoginForm />
       </h1>
     </div>
   );
