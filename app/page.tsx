60b1988cbc8d4a67fc39f1d40965bc54e0054abf
MorlonDev261 <mikandrapp@gmail.com>
2025-03-30 19:31:53 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { LoginButton } from "@/components/auth/login-button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-center items-center bg-gradient-to-t from-sky-400 via-blue-500 to-blue-800">
+    <main className="flex min-h-screen flex-col justify-center items-center bg-gradient-to-b from-sky-400 via-blue-500 to-blue-800">
       <div className="space-y-6 text-center">
         <h1 className="text-6xl font-bold text-white drop-shadow-md">Auth</h1>
         <p className="text-white text-lg text-opacity-90">
