ba198ee497638778b8fe13ac917111a1716b3218
MorlonDev261 <mikandrapp@gmail.com>
2025-03-30 17:15:04 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col justify-center items-center bg-gradient-radial from-sky-400 to-blue-800">
+    <main className="flex min-h-screen flex-col justify-center items-center bg-gradient-radial from-sky-400 to-blue-800">
       <div className="space-y-6 text-center">
-        <h1 className="text-6xl font-semibold text-white drop-shadow-md">Auth</h1>
-        <p className="text-white text-lg">
+        <h1 className="text-6xl font-bold text-white drop-shadow-md">Auth</h1>
+        <p className="text-white text-lg text-opacity-90">
           A simple authentication service
         </p>
         <div>
