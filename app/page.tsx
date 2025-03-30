5488c5ad895b7db6cf5f688b41f0722c3ba1634a
MorlonDev261 <mikandrapp@gmail.com>
2025-03-30 17:06:37 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col justify-center items-center bg[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <main className="flex h-full flex-col justify-center items-center bg-gradient-radial from-sky-400 to-blue-800">
       <div className="space-y-6 text-center">
         <h1 className="text-6xl font-semibold text-white drop-shadow-md">Auth</h1>
         <p className="text-white text-lg">
