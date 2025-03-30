350981fbf97657f0f8d583292b52cf70fc7ded41
MorlonDev261 <mikandrapp@gmail.com>
2025-03-30 18:14:15 +0300

    Update login-form.tsx

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,25 +1,5 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
-interface LoginFormProps {
-  children: React.ReactNode;
-  mode?: "modal" | "redirect",
-  asChild?: boolean;
-}
-
-export const LoginButton = ({ children, mode, asChild }: LoginButtonProps) => {
-  const router = useRouter();
-
-  const onClick = () => {
-    router.push("/auth/login");
-  };
-
-  if (mode === "modal") {
-    return (
-      <span>
-        Modal
-      </span>
-    )
-  }
+export const LoginButton = () => {
+ return (
+   <h2>Hello Formulaire</h2>
+ )
 }
