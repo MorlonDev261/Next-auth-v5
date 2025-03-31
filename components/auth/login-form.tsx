f7b6fb997b4c857f52416282e0d8ca5205af1ea3
MorlonDev261 <mikandrapp@gmail.com>
2025-03-31 15:11:25 +0300

    Update login-form.tsx

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,15 @@
+import { CardWrapper } from "./card-wrapper"
+
 const LoginForm = () => {
  return (
-   <h2>Hello Formulaire</h2>
+   <CardWrapper
+     headerLabel="Welcome back" 
+     backButtonLabel="Don't have an account?" 
+     backButtonHref="/auth/register"
+     showSocial
+   >
+     Hello Formulaire
+   </CardWrapper>
  )
 }
 
